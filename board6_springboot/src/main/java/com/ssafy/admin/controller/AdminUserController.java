39dc8fc02444af0e8e8ab420e0135802698ec6b6
EunSeo119 <qnrqjrm03@naver.com>
2023-05-01 17:37:08 +0900

    5/1

--- board6_springboot/src/main/java/com/ssafy/admin/controller/AdminUserController.java
+++ board6_springboot/src/main/java/com/ssafy/admin/controller/AdminUserController.java
@@ -23,10 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.member.model.MemberDto;
 import com.ssafy.member.model.service.MemberService;
 
-//@RestController
-@Controller
+import io.swagger.annotations.Api;
+
+@RestController
+//@Controller
 @RequestMapping("/admin")
 @CrossOrigin("*")
+@Api(tags = {"회원관리"})
 public class AdminUserController {
 
 	private static final Logger logger = LoggerFactory.getLogger(AdminUserController.class);
@@ -38,110 +41,110 @@ public class AdminUserController {
 		this.memberService = memberService;
 	}
 
-	@RequestMapping(value = "/user", method = RequestMethod.GET, headers = { "Content-type=application/json" })
-	public List<MemberDto> userList() throws Exception {
-		List<MemberDto> list = memberService.listMember(null);
-		logger.debug("회원목록 : {}", list);
-		return list;
-//        return memberService.listMember();
-	}
-
-	@RequestMapping(value = "/user", method = RequestMethod.POST, headers = { "Content-type=application/json" })
-	public List<MemberDto> userRegister(@RequestBody MemberDto memberDto) throws Exception {
-		memberService.joinMember(memberDto);
-		return memberService.listMember(null);
-	}
-
-	@RequestMapping(value = "/user/{userid}", method = RequestMethod.GET, headers = { "Content-type=application/json" })
-	public MemberDto userInfo(@PathVariable("userid") String userid) throws Exception {
-		return memberService.getMember(userid);
-	}
-
-	@RequestMapping(value = "/user", method = RequestMethod.PUT, headers = { "Content-type=application/json" })
-	public List<MemberDto> userModify(@RequestBody MemberDto memberDto) throws Exception {
-		memberService.updateMember(memberDto);
-		return memberService.listMember(null);
-	}
-
-	@RequestMapping(value = "/user/{userid}", method = RequestMethod.DELETE, headers = {
-			"Content-type=application/json" })
-	public List<MemberDto> userDelete(@PathVariable("userid") String userid) throws Exception {
-		memberService.deleteMember(userid);
-		return memberService.listMember(null);
-	}
-
-//	@GetMapping(value = "/user")
-//	public ResponseEntity<?> userList() {
-//		logger.debug("userList call");
-//		try {
-//			List<MemberDto> list = memberService.listMember(null);
-//			if(list != null && !list.isEmpty()) {
-//				return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-////				return new ResponseEntity<List<MemberDto>>(HttpStatus.NOT_FOUND);
-//			} else {
-//				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-//			}
-//		} catch (Exception e) {
-//			return exceptionHandling(e);
-//		}
-//		
-//	}
-//	
-//	@PostMapping(value = "/user")
-//	public ResponseEntity<?> userRegister(@RequestBody MemberDto memberDto) {
-//		logger.debug("userRegister memberDto : {}", memberDto);
-//		try {
-//			memberService.joinMember(memberDto);
-//			List<MemberDto> list = memberService.listMember(null);
-//			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-////			return new ResponseEntity<Integer>(cnt, HttpStatus.CREATED);
-//		} catch (Exception e) {
-//			return exceptionHandling(e);
-//		}
-//		
-//	}
-//	
-//	@GetMapping(value = "/user/{userid}")
-//	public ResponseEntity<?> userInfo(@PathVariable("userid") String userId) {
-//		logger.debug("userInfo userid : {}", userId);
-//		try {
-//			MemberDto memberDto = memberService.getMember(userId);
-//			if(memberDto != null)
-//				return new ResponseEntity<MemberDto>(memberDto, HttpStatus.OK);
-//			else
-//				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-//		} catch (Exception e) {
-//			return exceptionHandling(e);
-//		}
-//	}
-//	
-//	@PutMapping(value = "/user")
-//	public ResponseEntity<?> userModify(@RequestBody MemberDto memberDto) {
-//		logger.debug("userModify memberDto : {}", memberDto);
-//		try {
-//			memberService.updateMember(memberDto);
-//			List<MemberDto> list = memberService.listMember(null);
-//			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-//		} catch (Exception e) {
-//			return exceptionHandling(e);
-//		}
-//	}
-//	
-//	@DeleteMapping(value = "/user/{userid}")
-//	public ResponseEntity<?> userDelete(@PathVariable("userid") String userId) {
-//		logger.debug("userDelete userid : {}", userId);
-//		try {
-//			memberService.deleteMember(userId);
-//			List<MemberDto> list = memberService.listMember(null);
-//			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-//		} catch (Exception e) {
-//			return exceptionHandling(e);
-//		}
+//	@RequestMapping(value = "/user", method = RequestMethod.GET, headers = { "Content-type=application/json" })
+//	public List<MemberDto> userList() throws Exception {
+//		List<MemberDto> list = memberService.listMember(null);
+//		logger.debug("회원목록 : {}", list);
+//		return list;
+////        return memberService.listMember();
 //	}
 //
-//	private ResponseEntity<String> exceptionHandling(Exception e) {
-//		e.printStackTrace();
-//		return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//	@RequestMapping(value = "/user", method = RequestMethod.POST, headers = { "Content-type=application/json" })
+//	public List<MemberDto> userRegister(@RequestBody MemberDto memberDto) throws Exception {
+//		memberService.joinMember(memberDto);
+//		return memberService.listMember(null);
 //	}
+//
+//	@RequestMapping(value = "/user/{userid}", method = RequestMethod.GET, headers = { "Content-type=application/json" })
+//	public MemberDto userInfo(@PathVariable("userid") String userid) throws Exception {
+//		return memberService.getMember(userid);
+//	}
+//
+//	@RequestMapping(value = "/user", method = RequestMethod.PUT, headers = { "Content-type=application/json" })
+//	public List<MemberDto> userModify(@RequestBody MemberDto memberDto) throws Exception {
+//		memberService.updateMember(memberDto);
+//		return memberService.listMember(null);
+//	}
+//
+//	@RequestMapping(value = "/user/{userid}", method = RequestMethod.DELETE, headers = {
+//			"Content-type=application/json" })
+//	public List<MemberDto> userDelete(@PathVariable("userid") String userid) throws Exception {
+//		memberService.deleteMember(userid);
+//		return memberService.listMember(null);
+//	}
+
+	@GetMapping(value = "/user")
+	public ResponseEntity<?> userList() {
+		logger.debug("userList call");
+		try {
+			List<MemberDto> list = memberService.listMember(null);
+			if(list != null && !list.isEmpty()) {
+				return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+//				return new ResponseEntity<List<MemberDto>>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+		
+	}
+	
+	@PostMapping(value = "/user")
+	public ResponseEntity<?> userRegister(@RequestBody MemberDto memberDto) {
+		logger.debug("userRegister memberDto : {}", memberDto);
+		try {
+			memberService.joinMember(memberDto);
+			List<MemberDto> list = memberService.listMember(null);
+			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+//			return new ResponseEntity<Integer>(cnt, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+		
+	}
+	
+	@GetMapping(value = "/user/{userid}")
+	public ResponseEntity<?> userInfo(@PathVariable("userid") String userId) {
+		logger.debug("userInfo userid : {}", userId);
+		try {
+			MemberDto memberDto = memberService.getMember(userId);
+			if(memberDto != null)
+				return new ResponseEntity<MemberDto>(memberDto, HttpStatus.OK);
+			else
+				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+	@PutMapping(value = "/user")
+	public ResponseEntity<?> userModify(@RequestBody MemberDto memberDto) {
+		logger.debug("userModify memberDto : {}", memberDto);
+		try {
+			memberService.updateMember(memberDto);
+			List<MemberDto> list = memberService.listMember(null);
+			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+	@DeleteMapping(value = "/user/{userid}")
+	public ResponseEntity<?> userDelete(@PathVariable("userid") String userId) {
+		logger.debug("userDelete userid : {}", userId);
+		try {
+			memberService.deleteMember(userId);
+			List<MemberDto> list = memberService.listMember(null);
+			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+
+	private ResponseEntity<String> exceptionHandling(Exception e) {
+		e.printStackTrace();
+		return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
 }
